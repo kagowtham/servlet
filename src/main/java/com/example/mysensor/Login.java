9549f536d3ea58ac4a5ef6e2e716ee4d8650906b
kagowtham <ka.gow1996@gmail.com>
2018-02-11 22:06:03 +0530

    fixed bug

--- src/main/java/com/example/mysensor/Login.java
+++ src/main/java/com/example/mysensor/Login.java
@@ -56,7 +56,7 @@ public void init() throws ServletException {
 		if(usr.equals("a")&&pwd.equals("a")) {
 			collection = database.getCollection("images");
 			collection.deleteMany(new Document());
-			database.getCollection("profiles");
+			collection = database.getCollection("profiles");
 		}
 		List<DBObject> criteria = new ArrayList<DBObject>();
 		criteria.add(new BasicDBObject("usr",usr ));
